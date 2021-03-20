d3a0fa0618661d1e11e81b9ca9fb9726e11ad122
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-03-20 02:33:17 +0200

    Created Postits#edit (with view) and Postits#update methods. Added before_action :set_postit, only: [:edit, :update] Updated Postits#create:  - removed instance variable @colors from it because is not used in the view and placed it in a global constant  - update Postit.new() so that it uses strong params from postit_params and also edited that to only permit :description Styled views/postits/card 'Edit' link with an icon and position at the bottom of the card

--- app/controllers/postits_controller.rb
+++ app/controllers/postits_controller.rb
@@ -1,22 +1,43 @@
 class PostitsController < ApplicationController
+  before_action :set_postit, only: [:edit, :update]
+
+  # Define a colors constant
+  COLORS = ["#86A1DA", "#88D6BA", "#F23595", "#283139", "#FEE269"]
+
   def new
     @postit = Postit.new
   end
 
   def create
-    @color = ["#86A1DA", "#88D6BA", "#F23595", "#283139", "#FEE269"]
-    @postit = Postit.new(user: current_user, description: params[:postit][:description], color: @color.sample)
+    # Create a new Postit using strong params
+    @postit = Postit.new(postit_params)
+    # Add a random color from the COLORS array
+    @postit.color = COLORS.sample
+    # Set Postit user to current user
+    @postit.user = current_user
 
+    # Save and check if successful
     if @postit.save
+      # Redirect to postits#index
       redirect_to postits_path
+      # Flash with a nice message to confirm action success
       flash[:alert] = "Postit created Modafucka!"
     end
   end
 
   def edit
+    # Set post using id from params (before_action :set_postit)
   end
 
   def update
+    # Set post using id from params (before_action :set_postit)
+    # Update and check if postit was updated
+    if @postit.update(postit_params)
+      # Redirect to postits#index
+      redirect_to postits_path
+      # Flash with a nice message to confirm action success
+      flash[:alert] = "Postit updated Modafucka!"
+    end
   end
 
   def destroy
@@ -33,6 +54,11 @@ class PostitsController < ApplicationController
   private
 
   def postit_params
-    params.require(:postit).permit(:description, color: @color.sample)
+    params.require(:postit).permit(:description)
+  end
+
+  def set_postit
+    # Find Postit model using id from url params
+    @postit = Postit.find(params[:id])
   end
 end
